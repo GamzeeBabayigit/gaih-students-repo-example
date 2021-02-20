80b1d3e98e876e478b69c03c35479969c5adea2b
Gamze Babayiğit <47089443+GamzeeBabayigit@users.noreply.github.com>
2021-02-21 01:40:12 +0300

    HW1.py
    Generating a 3x3 matrix with random prime numbers(using for the for loop)

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,33 @@
-#Explain your work
+import random 
 
-#Question 1
-for x in range(a):
-	print(a)
+randNum=random.randint(1,1000)
+primes = []
+def primeNum(possiblePrime): 
+        randNum=random.randint(1,1000)
+        for num in range(2, possiblePrime):
+            if possiblePrime % num == 0:
+                return primeNum(randNum+1)
+            else:
+                return possiblePrime
+
+R = int(3) 
+C = int(3) 
+
+matrix = [] 
+  
+for i in range(R):		  
+	a =[] 
+	for j in range(C):	 
+
+          a.append(int(primeNum(randNum))) 
+	matrix.append(a) 
+
+# For printing the matrix 
+for i in range(R): 
+	for j in range(C): 
+		print( matrix[i][j])
+         
+	print("\n") 
+
+            
+
