6c9b724a6447a4d285c9e31f1cd5ea5a97bfe6c2
Gamze Babayiğit <47089443+GamzeeBabayigit@users.noreply.github.com>
2021-02-21 01:43:46 +0300

    HW3.py
    Write to function
    The name of the first function is prime_first.
    The name of the second function is prime_second.
    You must use these two function inside the for loop. Ensure that the for loop takes values between 0-1000.
    Press the prime numbers between 500-100  on the screen with the prime_second function.

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,28 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+
+def prime_first(possiblePrime): 
+        isPrime = True
+        for num in range(2, possiblePrime):
+            if possiblePrime % num == 0:
+                isPrime = False
+                break
+          
+        if isPrime:
+            print("Function First", possiblePrime)
+                 
+def prime_second(possiblePrime):
+        isPrime = True
+        for num in range(2, possiblePrime):
+            if possiblePrime % num == 0:
+                isPrime = False
+                break
+        if isPrime:
+            print("Function Second" ,possiblePrime)
+
+
+
+for i in range(2,1000):
+    if i<500:
+        prime_first(i)
+    else:
+        prime_second(i)
